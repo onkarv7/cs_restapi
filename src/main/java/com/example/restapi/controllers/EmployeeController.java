fe0350c466082d690163443182bcbe46f3220fb3
onkar <onkarvasav@gmail.com>
2026-06-09 17:03:28 +0530

    session 2.2

--- src/main/java/com/example/restapi/controllers/EmployeeController.java
+++ src/main/java/com/example/restapi/controllers/EmployeeController.java
@@ -1,15 +1,14 @@
 package com.example.restapi.controllers;
 
 import com.example.restapi.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 // req mapping, input and validations exception handling.
 // ResponseBody body + controller
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 
 
@@ -23,4 +22,15 @@ public class EmployeeController {
         return new EmployeeDTO(employeeID , "onkar", "ov@gmail.com", 27, LocalDate.of(1998, 10, 15),  true);
     }
 
+    @GetMapping
+    public String getEmployees(@RequestParam (required = false) Integer age,
+                               @RequestParam (required = false) String sortBy){
+        return  "age is " + age + sortBy;
+    }
+
+    @PostMapping
+    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO employee){
+        employee.setId(100L);
+        return employee;
+    }
 }
