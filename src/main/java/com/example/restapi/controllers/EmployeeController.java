4ddc29086ff14e0268c0d3ffa5a537308ca71799
onkar <onkarvasav@gmail.com>
2026-06-11 13:33:22 +0530

    session 2.3

--- src/main/java/com/example/restapi/controllers/EmployeeController.java
+++ src/main/java/com/example/restapi/controllers/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.example.restapi.controllers;
 
 import com.example.restapi.dto.EmployeeDTO;
+import com.example.restapi.entities.EmployeeEntity;
+import com.example.restapi.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 // req mapping, input and validations exception handling.
 // ResponseBody body + controller
@@ -11,26 +14,49 @@ import java.time.LocalDate;
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
 
     @GetMapping(path = "/")
     public String hello (){
         return "Hello World!";
     }
 
+//    @GetMapping("/{employeeID}")
+//    public EmployeeDTO getEmployeeById(@PathVariable Long employeeID){
+//        return new EmployeeDTO(employeeID , "onkar", "ov@gmail.com", 27, LocalDate.of(1998, 10, 15),  true);
+//    }
+
     @GetMapping("/{employeeID}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeID){
-        return new EmployeeDTO(employeeID , "onkar", "ov@gmail.com", 27, LocalDate.of(1998, 10, 15),  true);
+    public EmployeeEntity getEmployeeById(@PathVariable Long employeeID){
+        return employeeRepository.findById(employeeID).orElse(null);
     }
+
+//    @GetMapping
+//    public String getEmployees(@RequestParam (required = false) Integer age,
+//                               @RequestParam (required = false) String sortBy){
+//        return  "age is " + age + sortBy;
+//    }
 
     @GetMapping
-    public String getEmployees(@RequestParam (required = false) Integer age,
-                               @RequestParam (required = false) String sortBy){
-        return  "age is " + age + sortBy;
+    public List<EmployeeEntity> getEmployees(@RequestParam (required = false) Integer age,
+                                             @RequestParam (required = false) String sortBy){
+        return  employeeRepository.findAll();
     }
 
+
+//    @PostMapping
+//    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO employee){
+//        employee.setId(100L);
+//        return employee;
+//    }
+
     @PostMapping
-    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO employee){
-        employee.setId(100L);
-        return employee;
+    public EmployeeEntity createEmployee(@RequestBody EmployeeEntity employee){
+        return employeeRepository.save(employee);
     }
 }
--- src/main/java/com/example/restapi/entities.java
+++
@@ -1,4 +0,0 @@
-package com.example.restapi;
-
-public class entities {
-}
